793ff867864de06b83cf42b625e5b1c864cb6a50
Dunissimo <SuperDen214@yandex.ru>
2025-02-23 18:09:54 +0400

    feat: redesign

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,31 @@
 import './reset.scss';
 import './style.scss';
+
+const modalOpeners: NodeListOf<HTMLDivElement> = document.querySelectorAll('[data-modal-type]');
+const modalClosers = document.querySelectorAll('.modal__close');
+
+modalOpeners.forEach((node) => {
+    node.addEventListener('click', (e) => {
+        if (!(e.currentTarget instanceof HTMLElement)) return;
+
+        document.querySelectorAll('[data-type]').forEach((el => {
+            el.classList.remove('is-open');
+        }));
+
+        document.querySelector('.modal')?.classList.toggle('is-open');
+        document.querySelector(`[data-type='${e.currentTarget.dataset.modalType}']`)?.classList.toggle('is-open');
+    });
+});
+
+modalClosers.forEach((node) => {
+    node.addEventListener('click', (e) => {
+        if (!(e.currentTarget instanceof HTMLElement)) return;
+
+        if (e.currentTarget.parentElement) {
+            document.querySelector('.modal')?.classList.remove('is-open');
+            e.currentTarget.parentElement.classList.remove('is-open');
+
+        }
+    })
+})
+
